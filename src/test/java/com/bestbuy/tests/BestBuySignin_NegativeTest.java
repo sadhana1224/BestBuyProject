149db5c7ef60f51ff6c24e96395a21d0fa94c2a1
sadhana <sadhu.lokii1924@gmail.com>
2023-09-09 21:11:14 +0530

    added programs

--- src/test/java/com/bestbuy/tests/BestBuySignin_NegativeTest.java
+++ src/test/java/com/bestbuy/tests/BestBuySignin_NegativeTest.java
@@ -1,7 +1,14 @@
 package com.bestbuy.tests;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import com.bestbuy.utils.BestWrappers;
 import com.bestbuy.utils.Reports;
@@ -10,10 +17,47 @@ import com.bestbuy.utils.SelWrappers;
 public class BestBuySignin_NegativeTest extends SelWrappers {
 	SelWrappers se=new SelWrappers();
 	BestWrappers bw=new BestWrappers();
-	
-	@Test
-	
-	public void signIn_invalidCredentials() throws InterruptedException
+
+	@DataProvider(name="signIn_invalidCredentials")
+	public Object[][] readDataExcel() throws IOException
+	{
+		Object[][] data=null;
+		XSSFWorkbook workbook=null;
+		try
+		{
+			workbook= new XSSFWorkbook(System.getProperty("user.dir")+"/bestbuy.xlsx");	
+			//workbook= new XSSFWorkbook(new FileInputStream("C:\\Users\\Guest User\\OneDrive\\Desktop\\bestbuy.xlsx"));
+			XSSFSheet sheet= workbook.getSheet("testdata");
+			XSSFRow row= sheet.getRow(0);
+			
+			int noOfRows=sheet.getPhysicalNumberOfRows();
+			
+			int noOfCells= row.getPhysicalNumberOfCells();
+			
+			data= new Object[noOfRows-1][noOfCells]; //data[5][2]
+			
+			for(int i=1; i<noOfRows;i++)
+			{
+				for(int j=0; j<noOfCells;j++)
+				{
+					String cellValue=sheet.getRow(i).getCell(j).getStringCellValue();
+					data[i-1][j]=cellValue; 
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			workbook.close();
+		}
+		
+		return data;
+	}
+	@Test(dataProvider="signIn_invalidCredentials")
+	public void signIn_invalidCredentials(String mailId,String pwd) throws InterruptedException
 	{
 		try
 		{
@@ -21,10 +65,10 @@ public class BestBuySignin_NegativeTest extends SelWrappers {
 		bw.launchBrowser();
 		//bw.brokenLinkBestbuy();
 		se.chooseCountry();
-		bw.negativeTc_signIn("sadhuuuuu1223@gmaill.com","Bestbuy@123");
+		bw.negativeTc_signIn(mailId,pwd);
 		screenshot("invalidSignin");
 		Reports.setTCDesc("Sign in with valid username and empty field Password");
-		bw.negativeTc_signIn1("sadhanasuba24@test.com","");
+		//bw.negativeTc_signIn1("sadhanasuba24@test.com","");
 		screenshot("emptypwd");
 		}
 		catch(Exception ex)
--- src/test/java/com/bestbuy/tests/BestBuySignupTest.java
+++ src/test/java/com/bestbuy/tests/BestBuySignupTest.java
@@ -1,6 +1,12 @@
 package com.bestbuy.tests;
 
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.bestbuy.utils.BestWrappers;
@@ -10,9 +16,47 @@ import com.bestbuy.utils.Reports;
 public class BestBuySignupTest extends SelWrappers {
 	SelWrappers se=new SelWrappers();
 	BestWrappers bw=new BestWrappers();
-	
-	@Test
-	public void signUpPage()
+
+	@DataProvider(name="signIn_validCredentials")
+	public Object[][] readDataExcel() throws IOException
+	{
+		Object[][] data=null;
+		XSSFWorkbook workbook=null;
+		try
+		{
+			workbook= new XSSFWorkbook(System.getProperty("user.dir")+"/bestbuypos.xlsx");	
+			//workbook= new XSSFWorkbook(new FileInputStream("C:\\Users\\Guest User\\OneDrive\\Desktop\\bestbuy.xlsx"));
+			XSSFSheet sheet= workbook.getSheet("testdata");
+			XSSFRow row= sheet.getRow(0);
+			
+			int noOfRows=sheet.getPhysicalNumberOfRows();
+			
+			int noOfCells= row.getPhysicalNumberOfCells();
+			
+			data= new Object[noOfRows-1][noOfCells]; //data[5][2]
+			
+			for(int i=1; i<noOfRows;i++)
+			{
+				for(int j=0; j<noOfCells;j++)
+				{
+					String cellValue=sheet.getRow(i).getCell(j).getStringCellValue();
+					data[i-1][j]=cellValue; 
+				}
+			}
+		}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			workbook.close();
+		}
+		
+		return data;
+	}
+	@Test(dataProvider="signIn_validCredentials")
+	public void signUpPage(String mailId,String pwd)
 	{
 		try
 		{
@@ -20,7 +64,7 @@ public class BestBuySignupTest extends SelWrappers {
 			bw.launchBrowser();
 			Reports.setTCDesc("Validating SignUp functionality with valid credentials");
 		   bw.brokenLinkBestbuy();
-			bw.bestSignUp("sadhu","logesh","sadhanasuba24@test.com","One+two=3","One+two=3","8532498732","sadhanasuba24@test.com","One+two=3");
+			bw.bestSignUp("sadhu","logesh","sadhanasuba24@test.com","One+two=3","One+two=3","8532498732",mailId,pwd);
 			screenshot("signup_valid");
 		}
 		catch(Exception ex)
